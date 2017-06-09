d22ccd44ca73bd47bf0724d94d8e96cf9f195db7
mahikero <federicoparassarmiento@yahoo.com>
2017-06-09 14:22:25 -0400

    Initialize
    Basic Implenetatio of Angular to VS2013

---
+++ WebAPI/App_Start/BundleConfig.cs
@@ -0,0 +1,30 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace WebAPI
+{
+    public class BundleConfig
+    {
+        /// For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/polyfills").Include(
+                        "~/node_modules/core-js/client/shim.min.js",
+                        "~/node_modules/zone.js/dist/zone.min.js",
+                        "~/node_modules/systemjs/dist/system.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/node_modules/jquery/dist/jquery.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/node_modules/bootstrap/dist/js/bootstrap.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/node_modules/bootstrap/dist/css/bootstrap.min.css",
+                      "~/Content/styles.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/config").Include(
+                        "~/systemjs.config.js"));
+        }
+    }
+}
