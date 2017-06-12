4cd78193d2c186f82b563753ece403e9f7ff16da
Federico Paras Sarmiento <federicoparassarmiento@yahoo.com>
2017-06-12 10:53:30 +0800

    up

--- TransferFileFromAngular.bat
+++ TransferFileFromAngular.bat
@@ -5,3 +5,7 @@ xcopy D:\Proto\angular2-temp-quick\src\app\*.js D:\Proto\WebAPI\WebAPI\App /S /Y
 xcopy D:\Proto\angular2-temp-quick\src\app\*.html D:\Proto\WebAPI\WebAPI\App /S /Y
 
 xcopy D:\Proto\angular2-temp-quick\src\app\*.css D:\Proto\WebAPI\WebAPI\App /S  /Y 
+
+xcopy D:\Proto\angular2-temp-quick\src\app\*.css D:\Proto\WebAPI\WebAPI\App /S  /Y 
+
+xcopy D:\Proto\angular2-temp-quick\src\Content\*.css D:\Proto\WebAPI\WebAPI\Content /S  /Y 
